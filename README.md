f50e97a4f3a6fc4ffd15f7ea8be3bb089cc07d39
Geoff Fox <fox.geoffrey@gmail.com>
2020-12-22 15:08:04 -0800

    Display a component

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 1. Ref: <https://material.angular.io/>
 2. run ```ng add @angular/material```
+
+### Task:  Display a component
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSliderModule } from '@angular/material/slider';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -10,7 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
